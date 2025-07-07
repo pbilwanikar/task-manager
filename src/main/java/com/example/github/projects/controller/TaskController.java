e55db174af53167652650c8ce9fdd45df9ed5a2b
pbilwanikar <55834905+pbilwanikar@users.noreply.github.com>
2025-07-07 23:43:48 +0530

    get mapping for search by id

--- src/main/java/com/example/github/projects/controller/TaskController.java
+++ src/main/java/com/example/github/projects/controller/TaskController.java
@@ -6,10 +6,9 @@ import com.example.github.projects.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/v1/api")
@@ -24,7 +23,14 @@ public class TaskController {
         return ResponseEntity.status(HttpStatus.CREATED).body(savedTask);
     }
 
-
+    @GetMapping("/getTask/{id}")
+    public ResponseEntity<Optional<Task>> getTaskById(@PathVariable Long id){
+        Optional<Task> taskFound = taskService.getTask(id);
+        if (taskFound==null){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(taskFound);
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(taskFound);
+    }
 
 }
 
